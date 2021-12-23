d75d0b1818580d1ab81a1b8fd8e1db17c05fd85b
Vamshi Maddur <marvelousdev.0@gmail.com>
2021-12-22 19:38:06 -0500

    add typefaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Counter } from './Counter'
+import './App.css'
 
 export const App = () => {
   return (
-    <>
+    <div className="App">
       <h1>React TypeScript Webpack Starter Template</h1>
-      <Counter />
-    </>
+    </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import '@fontsource/plus-jakarta-sans'
+import '@fontsource/ibm-plex-sans'
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
